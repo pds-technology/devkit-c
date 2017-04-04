57046667a653c4775caae32ad5f4edef30347ecb
Bobby Diaz <bobby.diaz@pds.nl>
2017-04-03 22:24:25 -0500

    Added generated classes for PRODML 2.0 and RESQML 2.1
    Related Work Items: #10903

---
+++ source/DevKitGenerator/DataAccess/BaseEnumExtension.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Xml;
+namespace Energistics.DataAccess
+{
+    /// <summary>
+    /// extension serialize the string.
+    /// </summary>
+    [Serializable]
+    public abstract class BaseEnumExtension :Object ,IXmlSerializable
+    {  
+        public abstract String EnumValueStr
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// construction
+        /// </summary>
+        public BaseEnumExtension()
+            : base()
+            {
+            } 
+              
+             String customerExtEnumStr = "";
+            
+            /// <summary>
+            /// the customer enum extension string.
+            /// </summary>
+            [XmlIgnore]
+             public String CustomerExtEnumStr
+             {
+                 get { return customerExtEnumStr; }
+                 set { customerExtEnumStr = value; }
+             
+             }
+            
+            /// <summary>
+            /// get the schema 
+            /// </summary>
+            /// <returns></returns>
+            public System.Xml.Schema.XmlSchema GetSchema()
+            {
+                return (null);
+            }
+
+            /// <summary>
+            /// read the xml
+            /// </summary>
+            /// <param name="reader"></param>
+            public new void ReadXml(XmlReader reader)
+            {
+                reader.MoveToContent();
+                Boolean isEmptyElement = reader.IsEmptyElement; // (1)
+                reader.ReadStartElement();
+                if (!isEmptyElement) // (1)
+                {
+                    String str = reader.ReadContentAsString();
+                    if(str.Contains(":")) //valide extension string.
+                    {
+                        this.customerExtEnumStr = str;
+                    }
+                    else
+                    {
+                        EnumValueStr = str;
+                        if (EnumValueStr != null)
+                            this.customerExtEnumStr = "";
+                    }
+                    reader.ReadEndElement();
+                }
+            }
+
+            /// <summary>
+            /// write the xml.
+            /// </summary>
+            /// <param name="writer"></param>
+            public new void WriteXml(XmlWriter writer)
+            {
+                if (customerExtEnumStr.Length>0)
+                    writer.WriteString(this.customerExtEnumStr);
+                else
+                    writer.WriteString(EnumValueStr);
+            }
+    }
+}
